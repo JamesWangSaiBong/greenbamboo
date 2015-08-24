fd4027a5132bbf5968f03bb5478a76218705dc7e
James Wang <jwsb0702@gmail.com>
2015-08-24 13:01:32 -0400

    Toggle language in menu-section title

--- app/menu/directives/menuSectionDir.js
+++ app/menu/directives/menuSectionDir.js
@@ -5,17 +5,32 @@ app.directive('gbMenuSection', function() {
 		restrict: 'E',
 		transclude: true,
 		scope: {
-			title: '@'
+			title: '@',
+			cnTitle: '@'
 		},
 		require: '^gbMenu',
 		templateUrl: 'menu/directives/menuSectionDir.html',
 		link: function(scope, el, attrs, gbMenuCtrl) {
+			
+			scope.displayTitle = scope.title;
 			
 			gbMenuCtrl.addSection(scope);
 			
 			scope.select = function() {
 				gbMenuCtrl.select(scope);
 			}
+		},
+		controller: function($scope) {
+			$scope.$on('CHANGE_LANG', function(event, lang) {
+				switch (lang) {
+					case 'cn':
+						$scope.displayTitle = $scope.cnTitle;
+						break;
+					case 'en':
+						$scope.displayTitle = $scope.title;
+						break;
+				}
+			})
 		}
 	}
 })
