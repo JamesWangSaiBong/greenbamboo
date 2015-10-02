5a51a5ca139614a5df69525bf16193756bdf5c0f
James Wang <jwsb0702@gmail.com>
2015-10-02 00:09:04 -0400

    Renamed images, add alt attributes and off-screen headers for SEO

--- app/partials/headerSlideDir.js
+++ app/partials/headerSlideDir.js
@@ -8,14 +8,14 @@ app.directive('gbHeaderSlides', function() {
 			$scope.noWrap = false;
 			
 			$scope.slides = [{
-				image: '_assets/beef-roll.jpg',
+				image: '_assets/vietnamese-meat-wrap-roll.jpg',
 				text: ''
 			}, {
-				image: '_assets/spring-roll.jpg',
+				image: '_assets/vietnamese-spring-roll.jpg',
 				text: ''
 			},
 			{
-				image: '_assets/thai-beef-noodle.jpg',
+				image: '_assets/thai-beef-pho.jpg',
 				text: ''
 			}]
 		}
